0d89a53054ff5f0607889d2abb47dbe5733e0f64
Thiệu Trung Kiên <87187870+TrunqKj3n@users.noreply.github.com>
2023-02-27 09:20:51 +0700

    Update upload.js

--- routes/upload.js
+++ routes/upload.js
@@ -1,54 +1,47 @@
 const fs = require('fs-extra');
-const upload = require('../handler/mongodb.js');
+const Upload = require('../handler/mongodb.js');
 const config = require('../config.js');
-class uploadFile {
+
+class UploadFile {
   constructor() {
     this.name = 'uploadFile';
     this.method = 'POST';
-    this.handler = this.handler;
+    this.handler = this.handler.bind(this);
   }
+
   async handler(req, res) {
-    var { type, data, code } = req.body;
-    if (type && data && code) {
+    const { type, data, code } = req.body;
+
+    if (!type || !data || !code) {
+      return res.send({ status: 400, message: 'Missing required fields' });
+    }
+
+    try {
+      let link;
       switch (config.type) {
         case 'mongodb':
-          var uploadPhoto = new upload({
-            type: type,
-            code: code,
-            data: data
-          });
-          uploadPhoto.save(function (err, uploadPhoto) {
-            if (err) return console.error(err);
-            res.send({
-              status: 200,
-              message: 'Upload thành công',
-              link : `photo/${code}`
-            });
-          });
+          const uploadPhoto = new Upload({ type, code, data });
+          const savedPhoto = await uploadPhoto.save();
+          link = `photo/${savedPhoto.code}`;
           break;
+
         case 'json':
-          var json = fs.readFileSync('./cache/data.json');
-          var obj = JSON.parse(json);
-          obj.push({
-            type: type,
-            code: code,
-            data: data
-          });
+          const json = fs.readFileSync('./cache/data.json');
+          const obj = JSON.parse(json);
+          obj.push({ type, code, data });
           fs.writeFileSync('./cache/data.json', JSON.stringify(obj));
-          res.send({
-            status: 200,
-            message: 'Upload thành công',
-            link: `photo/${code}`
-          });
+          link = `photo/${code}`;
           break;
+
         default:
-          res.send({
-            status: 500,
-            message: 'Lỗi hệ thống'
-          });
-          break;
+          return res.send({ status: 500, message: 'Internal server error' });
       }
+
+      res.send({ status: 200, message: 'Upload thành công', link });
+    } catch (error) {
+      res.send({ status: 500, message: error.message });
     }
   }
 }
-module.exports = new uploadFile();
+
+module.exports = new UploadFile();
