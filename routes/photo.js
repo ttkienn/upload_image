effcd6da3d75cc5c1f6da4c119ca374b5872e6b4
Thiệu Trung Kiên <87187870+TrunqKj3n@users.noreply.github.com>
2023-02-27 09:18:52 +0700

    Update photo.js

--- routes/photo.js
+++ routes/photo.js
@@ -1,50 +1,49 @@
-const fs = require("fs-extra"),
-    config = require("../config"),
-    Mongoose = require("../handler/mongodb");
-class photo {
+const fs = require("fs-extra");
+const config = require("../config");
+const Mongoose = require("../handler/mongodb");
+const _ = require("lodash");
+
+class Photo {
     constructor() {
-        this.name = "photo/:type",
-        this.method = "GET",
-        this.handler = this.handler
+        this.name = "photo/:type";
+        this.method = "GET";
+        this.handler = this.handler.bind(this);
     }
-    handler(req, res) {
-        var type = req.params.type;
-        switch (config.type) {
-            case "mongodb":
-                Mongoose.find({
-                    code: type
-                }, (function (err, docs) {
-                    if (err) return console.error(err);
-                    if (0 == docs.length) res.send({
-                        status: 404,
-                        message: "Không tìm thấy ảnh"
-                    });
-                    else {
-                        var img = Buffer.from(docs[0].data.split(",")[1], "base64");
-                        res.writeHead(200, {
-                            "Content-Type": "image/png",
-                            "Content-Length": img.length
-                        }), res.end(img)
-                    }
-                }));
-                break;
-            case "json":
-                var json = fs.readFileSync("./cache/data.json"),
-                    data = JSON.parse(json).filter((function (item) {
-                        return item.code == type
-                    })),
-                    img = Buffer.from(data[0].data.split(",")[1], "base64");
-                res.writeHead(200, {
-                    "Content-Type": "image/png",
-                    "Content-Length": img.length
-                }), res.end(img);
-                break;
-            default:
-                res.send({
-                    status: 500,
-                    message: "Lỗi hệ thống"
-                })
+
+    async handler(req, res) {
+        try {
+            const type = req.params.type;
+
+            let data;
+            switch (config.type) {
+                case "mongodb":
+                    data = await Mongoose.find({ code: type }).exec();
+                    break;
+                case "json":
+                    const json = fs.readFileSync("./cache/data.json");
+                    data = JSON.parse(json).filter(item => item.code === type);
+                    break;
+                default:
+                    throw new Error("Lỗi hệ thống");
+            }
+
+            if (_.isEmpty(data)) {
+                return res.status(404).json({ status: 404, message: "Không tìm thấy ảnh" });
+            }
+
+            const img = Buffer.from(data[0].data.split(",")[1], "base64");
+
+            res.writeHead(200, {
+                "Content-Type": "image/png",
+                "Content-Length": img.length
+            });
+
+            res.end(img);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: true, message: error.message });
         }
     }
 }
-module.exports = new photo;
+
+module.exports = new Photo();
